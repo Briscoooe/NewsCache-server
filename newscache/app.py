26d1aba87671768083e58ed35848b7b9a315dd1d
Brian Briscoe <brianbriscoe06@gmail.com>
2018-03-22 16:23:20 -0400

    Added newsapi client

--- newscache/app.py
+++ newscache/app.py
@@ -1,31 +1,28 @@
 import os
 from functools import wraps
 from flask import Flask, abort, jsonify, request
+from newsapi import NewsApiClient
+
+
 app = Flask(__name__)
-
-incomes = [
-  { 'description': 'salary', 'amount': 5000 }
-]
-
 api_key = os.environ['NEWS_API_KEY'] 
+api_key_header_name = 'X-Api-Key'
+newsapi = NewsApiClient(api_key=api_key)
 
 def requires_key(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        if request.headers.get('news-api-key') and request.headers.get('news-api-key') == api_key: 
+        if request.headers.get(api_key_header_name) and request.headers.get(api_key_header_name) == api_key: 
             return f(*args, **kwargs)
         else:
             abort(401)
     return decorated
 
-@app.route('/test')
-def test():
-    return 'OK'
-
-@app.route('/incomes', methods=['POST'])
+@app.route('/sources/', methods=['POST'])
 @requires_key
-def add_income():
-    return 'Authenticated', 200
+def get_sources():
+    sources = newsapi.get_sources()
+    return jsonify(sources), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
